0c01b23ed18f145f6d0a4e9dab434d081902bc0b
Sebastian <sebastian.windmueller@gmail.com>
2020-06-28 12:40:12 +0200

    Added persistent file storage #2

--- docker/price_predictor.py
+++ docker/price_predictor.py
@@ -1,4 +1,5 @@
-
+import pandas as pd
+from joblib import dump, load
 # from sklearn.ensemble import RandomForestRegressor
 # model = RandomForestRegressor()
 # model.fit(X_train, Y_train)
@@ -23,3 +24,36 @@
 # print("Training test score: ",str(model.score(X_train_scaled, Y_train)))
 # print("Test set score: ", str(model.score(X_test_scaled, Y_test)))
 
+
+def prepare_model(verbose=False):
+    from sklearn.model_selection import train_test_split
+    dataframe = pd.read_pickle("./apartments_dataframe.pkl")
+
+    y = dataframe['rent']
+    X = dataframe[['living_space', 'number_rooms']]
+    X_train, X_test, Y_train, Y_test = train_test_split(X, y, random_state=0)
+
+    if verbose is True:
+        print("Shape of dataframes: ", X_train.shape, X_test.shape, Y_train.shape, Y_test.shape)
+    return X_train, X_test, Y_train, Y_test
+
+
+def learn_linear_model(X_train, X_test, Y_train, Y_test, verbose=False):
+    from sklearn import linear_model
+
+    model_object = linear_model.LinearRegression().fit(X_train, Y_train)
+
+    if verbose is True:
+        print("Training set score: ", str(model_object.score(X_train, Y_train)))
+        print("Test set score: ", str(model_object.score(X_test, Y_test)))
+    dump(model_object, './apartment_model.joblib')
+    return
+
+
+def predict_apartment_price(living_space, number_of_rooms, verbose=False):
+    model_object = load('./apartment_model.joblib')
+    X_new = pd.DataFrame([[living_space, number_of_rooms]])
+    prediction = model_object.predict(X_new)[0].round(2)
+    if verbose is True:
+        print("\nAn apartment with " + str(number_of_rooms) + " rooms and " + str(living_space) + " m² costs " + str(
+            prediction) + " € per month.\n")
--- docker/webscraper.py
+++ docker/webscraper.py
@@ -5,93 +5,71 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 import pandas as pd
 
-base_URL = 'https://www.immobilienscout24.de/Suche/de/baden-wuerttemberg/stuttgart/west/wohnung-mit-einbaukueche-mieten?haspromotion=false&sorting=2&pagenumber='
-base_URL = 'https://www.immobilienscout24.de/Suche/de/baden-wuerttemberg/stuttgart/wohnung-mit-einbaukueche-mieten?haspromotion=false&sorting=2&pagenumber='
 
-page = requests.get(str(base_URL) + str(1))
-soup = BeautifulSoup(page.content, 'html.parser')
-number_of_results_pages = len(soup.find('select', class_="select").find_all('option'))
-time.sleep(1)
+def scrape_data_from_immoscout(verbose=False):
+    """
+    Collect data on apartments from immobilienscout24.de and return the collected data as a Pandas dataframe
+    :param verbose: boolean, set True to display console output
+    :return: Pandas dataframe containing apartment data as seen in offer_data
+    """
+    base_URL = 'https://www.immobilienscout24.de/Suche/de/baden-wuerttemberg/stuttgart/west/wohnung-mit-einbaukueche-mieten?haspromotion=false&sorting=2&pagenumber='
+    base_URL = 'https://www.immobilienscout24.de/Suche/de/baden-wuerttemberg/stuttgart/wohnung-mit-einbaukueche-mieten?haspromotion=false&sorting=2&pagenumber='
 
-list_of_all_offer_data = []
-
-for results_page in range(number_of_results_pages):
-
-    new_URL = str(base_URL) + str(results_page + 1)
-    page = requests.get(new_URL)
+    page = requests.get(str(base_URL) + str(1))
     soup = BeautifulSoup(page.content, 'html.parser')
-    results_list_container = soup.find('ul', id="resultListItems")
-    all_offers = results_list_container.find_all('li', class_="result-list__listing")
+    number_of_results_pages = len(soup.find('select', class_="select").find_all('option'))
+    time.sleep(1)
 
-    for offer in all_offers:
-        offer_id = offer['data-id']
-        access_date = datetime.today().strftime('%Y-%m-%d')
-        link_prefix = 'https://www.immobilienscout24.de'
-        offer_link = link_prefix + offer.find('a')['href']
+    list_of_all_offer_data = []
 
-        offer_details = offer.find_all('div', class_="grid grid-flex gutter-horizontal-l gutter-vertical-s")
-        offer_details = offer.find_all('dd', class_="font-nowrap font-highlight font-tabular")
+    for results_page in range(number_of_results_pages):
 
-        offer_rent = offer_details[0].text.replace("€", "").replace(".", "").replace(",", ".")
-        offer_living_space = offer_details[1].text.replace("m²", "").replace(",", ".")
-        offer_number_of_rooms = offer_details[2].find('span', class_="onlyLarge").text.replace(",", ".")
+        new_URL = str(base_URL) + str(results_page + 1)
+        page = requests.get(new_URL)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        results_list_container = soup.find('ul', id="resultListItems")
+        all_offers = results_list_container.find_all('li', class_="result-list__listing")
 
-        offer_data = {
-            "offer_id": offer_id,
-            "access_date": access_date,
-            "link": offer_link,
-            "rent": offer_rent,
-            "living_space": offer_living_space,
-            "number_rooms": offer_number_of_rooms
-        }
+        for offer in all_offers:
+            offer_id = offer['data-id']
+            access_date = datetime.today().strftime('%Y-%m-%d')
+            link_prefix = 'https://www.immobilienscout24.de'
+            offer_link = link_prefix + offer.find('a')['href']
 
-        list_of_all_offer_data.append(offer_data)
-    if results_page % 10 == 0:
-        print("finished page " + str(results_page + 1) + " of " + str(number_of_results_pages))
-    time.sleep(0.1)
+            offer_details = offer.find_all('div', class_="grid grid-flex gutter-horizontal-l gutter-vertical-s")
+            offer_details = offer.find_all('dd', class_="font-nowrap font-highlight font-tabular")
 
-all_offers_dataframe = pd.DataFrame(list_of_all_offer_data)
+            offer_rent = offer_details[0].text.replace("€", "").replace(".", "").replace(",", ".")
+            offer_living_space = offer_details[1].text.replace("m²", "").replace(",", ".")
+            offer_number_of_rooms = offer_details[2].find('span', class_="onlyLarge").text.replace(",", ".")
 
-print("Apartment offerings stored: ", str(all_offers_dataframe.shape[0]))
+            offer_data = {
+                "offer_id": offer_id,
+                "access_date": access_date,
+                "link": offer_link,
+                "rent": offer_rent,
+                "living_space": offer_living_space,
+                "number_rooms": offer_number_of_rooms
+            }
 
+            list_of_all_offer_data.append(offer_data)
+        if verbose is True:
+            if results_page % 10 == 0:
+                print("finished page " + str(results_page + 1) + " of " + str(number_of_results_pages))
+        time.sleep(0.1)
 
-# Optional check for duplicates
-if all_offers_dataframe[all_offers_dataframe.duplicated(['offer_id'])].empty == False:
-    print("Duplicates found")
-else:
-    print("No duplicates found")
+    all_offers_dataframe = pd.DataFrame(list_of_all_offer_data)
+    if verbose is True:
+        print("Apartment offerings stored: ", str(all_offers_dataframe.shape[0]))
+        # Check for duplicates
+        if all_offers_dataframe[all_offers_dataframe.duplicated(['offer_id'])].empty == False:
+            print("Duplicates found")
+        else:
+            print("No duplicates found")
 
-
-# TODO:
-#  save files to csv
-# make duplicate check remove duplicates
-# fix mlp regressor
-# add k-fold cross validation
-# increase accuracy
-# modularize web scraping and the prediction model
-# .
-
-
-from sklearn.model_selection import train_test_split
-
-
-y = all_offers_dataframe['rent']
-X = all_offers_dataframe[['living_space', 'number_rooms']]
-X_train, X_test, Y_train, Y_test = train_test_split(X, y, random_state=0)
-print(X_train.shape, X_test.shape, Y_train.shape, Y_test.shape)
-
-from sklearn import linear_model
+    # Save dataframe to disk
+    all_offers_dataframe.to_pickle("./apartments_dataframe.pkl")
+    return all_offers_dataframe
 
 
-model = linear_model.LinearRegression().fit(X_train, Y_train)
-print("Training test score: ", str(model.score(X_train, Y_train)))
-print("Test set score: ", str(model.score(X_test, Y_test)))
 
-# Enter the values for the new apartment:
-living_space = 45  # Living space in m²
-number_of_rooms = 2  # Number of rooms
-
-X_new = pd.DataFrame([[living_space, number_of_rooms]])
-
-print("An apartment with " + str(number_of_rooms) + " rooms and " + str(living_space) + " m² costs " + str(
-    model.predict(X_new)[0].round(2)) + " € per month.")
